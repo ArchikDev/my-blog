16004a169ebf06486958015d655cd63324809eda
a.galeev <a.galeev@malltech.ru>
2023-10-19 18:28:15 +0500

    add screens posts, post

--- app/src/main/java/ru/archik/my_blog/presentation/MyBlogApp.kt
+++ app/src/main/java/ru/archik/my_blog/presentation/MyBlogApp.kt
@@ -7,10 +7,13 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import ru.archik.my_blog.presentation.screens.AuthScreen
+import ru.archik.my_blog.presentation.screens.PostScreen
+import ru.archik.my_blog.presentation.screens.PostsScreen
 
 @Composable
 fun MyBlogApp() {
   Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-    AuthScreen()
+//    AuthScreen()
+    PostScreen()
   }
 }
---
+++ app/src/main/java/ru/archik/my_blog/presentation/screens/PostsScreen.kt
@@ -0,0 +1,99 @@
+package ru.archik.my_blog.presentation.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import ru.archik.my_blog.R
+
+@Composable
+fun PostsScreen() {
+  Scaffold {
+    LazyColumn(modifier = Modifier.padding(start = 15.dp, end = 15.dp)) {
+      items(5) {
+        Post()
+      }
+//      items(MockData.RestauranList) { RestauranData ->
+//        itemRestaurants(restauranData = RestauranData, onRestauranClick = {
+//          navController.navigate("${Constants.Screens.RESTAURAN_DETAIL_LIST_SCREEN}/${RestauranData.id}")
+////          navController.navigate(NavRoute.MyOrder.route)
+//        })
+//      }
+    }
+  }
+}
+
+@Composable
+private fun Post() {
+  Card(modifier = Modifier
+    .fillMaxWidth()
+    .padding(top = 7.dp, bottom = 15.dp, start = 15.dp, end = 15.dp)
+    .clickable {
+      // TODO
+    },
+    elevation = 10.dp
+  ) {
+    Column {
+      Image(
+        painter = painterResource(id = R.drawable.img_1),
+        contentDescription = "image",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+          .fillMaxWidth()
+          .height(230.dp)
+      )
+      Box(modifier = Modifier
+        .padding(
+          top = 15.dp,
+          start = 15.dp,
+          end = 15.dp,
+          bottom = 15.dp
+        )) {
+        Column {
+          Row(
+            modifier = Modifier
+              .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+          ) {
+            Text(
+              text = "Заголовок",
+              fontSize = 18.sp,
+              fontWeight = FontWeight.Bold
+            )
+            Text(
+              text = "11.10.2023:12:15",
+              fontSize = 14.sp,
+              fontWeight = FontWeight.Medium,
+              color = Color.Gray
+            )
+          }
+          Spacer(modifier = Modifier.height(15.dp))
+          Text(
+            text = "lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem",
+            fontSize = 16.sp,
+          )
+          Spacer(modifier = Modifier.height(15.dp))
+          Text(
+            text = "Тип новости: Важная",
+            fontSize = 16.sp,
+            color = Color.Gray
+          )
+        }
+      }
+    }
+  }
+}
